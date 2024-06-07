98146400d1a17562410e180a7348805123cdd384
yeshwanthkai <33931962+yeshwanthkai@users.noreply.github.com>
2024-06-07 12:18:21 +0530

    changed options

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Emacs")
-option_b = os.getenv('OPTION_B', "Vi")
+option_a = os.getenv('OPTION_A', "Akira")
+option_b = os.getenv('OPTION_B', "Kiyoshi")
 hostname = socket.gethostname()
-version = 'v1'
+version = 'v1.0.2'
 
 app = Flask(__name__)
 
